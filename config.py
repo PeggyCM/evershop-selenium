52c6673425674a695bdd1aff33f66100ca58d733
Peggy <pcombeaumansour@gmail.com>
2025-06-24 08:20:16 +0200

    creation de fonctions

---
+++ config.py
@@ -0,0 +1,17 @@
+BASE_URL = "http://localhost:3000/admin/login"
+
+VALID_EMAIL = "admin@admin.com"
+VALID_PASSWORD = "DB123456"
+
+INVALID_EMAIL = "admin@admi.com"
+INVALID_PASSWORD = "DB12345"
+
+#pour crea_categ
+CATEGORY_NAME = ["Homme","Femme","Enfant"] 
+SSCATEGORY_NAME = ["Vetements","Chaussures","Accessoires"]
+URL_KEY = "URL"
+FILE_PATH = "/home/combeau-mansour/Tools/EverShopDocker-TP/cintre.avif"
+
+#pour modif_categ
+CATEGORY_NAME_old = "Homme"
+CATEGORY_NAME_new = "Hommes" 
--- test_lab_login.py
+++ test_lab_login.py
@@ -6,15 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait  # Pour attendre les él
 from selenium.webdriver.support import expected_conditions as EC  # Pour définir les conditions d'attente
 from selenium.webdriver.chrome.service import Service  # Pour configurer le service Chrome
 from webdriver_manager.chrome import ChromeDriverManager  # Pour gérer automatiquement le driver Chrome
+import config as cfg    
 
 class TestLogin:
-    # Variables de configuration pour les tests
-    BASE_URL = "http://localhost:3000/admin/login"  # URL de la page de connexion
-    VALID_EMAIL = "admin@admin.com"  # Email valide pour la connexion
-    VALID_PASSWORD = "DB123456"  # Mot de passe valide pour la connexion
-    INVALID_EMAIL = "admin@admi.com"  # Email invalide pour tester les erreurs
-    INVALID_PASSWORD = "DB12345"  # Mot de passe invalide pour tester les erreurs
-
     @pytest.fixture
     def driver(self):
         # Configuration du driver Chrome avant chaque test
@@ -24,43 +18,43 @@ class TestLogin:
         yield driver  # Retourne le driver pour les tests
         driver.quit()  # Ferme le navigateur après les tests
 
+    def login_success(self, driver):
+        """Fonction réutilisable pour faire une connexion réussie"""
+        driver.get(cfg.BASE_URL)
+
+        email_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, "email"))
+        )
+        email_input.send_keys(cfg.VALID_EMAIL)
+
+        password_input = driver.find_element(By.NAME, "password")
+        password_input.send_keys(cfg.VALID_PASSWORD)
+
+        sign_in_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+        sign_in_button.click()
+
+        dashboard_heading = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "h1.page-heading-title"))
+        )
+        assert dashboard_heading.text == "Dashboard"
+
     def test_successful_login(self, driver):
-        # Test de connexion réussie
-        driver.get(self.BASE_URL)  # Navigation vers la page de connexion
-        
-        # Attente et remplissage de l'email
-        email_input = WebDriverWait(driver, 10).until(  # Attend jusqu'à 10 secondes
-            EC.presence_of_element_located((By.NAME, "email"))  # Pour que l'élément email soit présent
-        )
-        email_input.send_keys(self.VALID_EMAIL)  # Saisit l'email valide
-        
-        # Remplissage du mot de passe
-        password_input = driver.find_element(By.NAME, "password")  # Trouve le champ mot de passe
-        password_input.send_keys(self.VALID_PASSWORD)  # Saisit le mot de passe valide
-        
-        # Clic sur le bouton de connexion
-        sign_in_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")  # Trouve le bouton
-        sign_in_button.click()  # Clique sur le bouton
-        
-        # Vérification de la connexion réussie
-        dashboard_heading = WebDriverWait(driver, 10).until(  # Attend jusqu'à 10 secondes
-            EC.presence_of_element_located((By.CSS_SELECTOR, "h1.page-heading-title"))  # Pour que le titre Dashboard soit présent
-        )
-        assert dashboard_heading.text == "Dashboard"  # Vérifie que le titre est bien "Dashboard"
+        self.login_success(driver)
+
 
     def test_invalid_email(self, driver):
         # Test avec un email invalide
-        driver.get(self.BASE_URL)  # Navigation vers la page de connexion
+        driver.get(cfg.BASE_URL)  # Navigation vers la page de connexion
         
         # Attente et remplissage de l'email invalide
         email_input = WebDriverWait(driver, 10).until(  # Attend jusqu'à 10 secondes
             EC.presence_of_element_located((By.NAME, "email"))  # Pour que l'élément email soit présent
         )
-        email_input.send_keys(self.INVALID_EMAIL)  # Saisit l'email invalide
+        email_input.send_keys(cfg.INVALID_EMAIL)  # Saisit l'email invalide
         
         # Remplissage du mot de passe valide
         password_input = driver.find_element(By.NAME, "password")  # Trouve le champ mot de passe
-        password_input.send_keys(self.VALID_PASSWORD)  # Saisit le mot de passe valide
+        password_input.send_keys(cfg.VALID_PASSWORD)  # Saisit le mot de passe valide
         
         # Clic sur le bouton de connexion
         sign_in_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")  # Trouve le bouton
@@ -74,17 +68,17 @@ class TestLogin:
 
     def test_invalid_password(self, driver):
         # Test avec un mot de passe invalide
-        driver.get(self.BASE_URL)  # Navigation vers la page de connexion
+        driver.get(cfg.BASE_URL)  # Navigation vers la page de connexion
         
         # Attente et remplissage de l'email valide
         email_input = WebDriverWait(driver, 10).until(  # Attend jusqu'à 10 secondes
             EC.presence_of_element_located((By.NAME, "email"))  # Pour que l'élément email soit présent
         )
-        email_input.send_keys(self.VALID_EMAIL)  # Saisit l'email valide
+        email_input.send_keys(cfg.VALID_EMAIL)  # Saisit l'email valide
         
         # Remplissage du mot de passe invalide
         password_input = driver.find_element(By.NAME, "password")  # Trouve le champ mot de passe
-        password_input.send_keys(self.INVALID_PASSWORD)  # Saisit le mot de passe invalide
+        password_input.send_keys(cfg.INVALID_PASSWORD)  # Saisit le mot de passe invalide
         
         # Clic sur le bouton de connexion
         sign_in_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")  # Trouve le bouton
